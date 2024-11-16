7a4b72afa5600415b59dc4cc038b9bdeaa64467a
Анна <vindanka@gmail.com>
2024-11-16 22:56:14 +0200

    Create business logic to add, apdate, filter and delete book

---
+++ LibraryUoW/Services/LibraryService.cs
@@ -0,0 +1,57 @@
+﻿using LibraryUoW.Models;
+using LibraryUoW.UnitOfWork;
+
+namespace LibraryUoW.Services
+{
+    public class LibraryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LibraryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddBook(string title, int authorId, int genreId, int publishedYear)
+        {
+            _unitOfWork.Books.Add(new Book
+            {
+                Title = title,
+                AuthorId = authorId,
+                GenreId = genreId,
+                PublishedYear = publishedYear
+            });
+            _unitOfWork.Commit();
+        }
+
+        public void UpdateBook(int id, string title, int authorId, int genreId, int publishedYear)
+        {
+            var book = _unitOfWork.Books.GetById(id);
+            if (book != null)
+            {
+                book.Title = title;
+                book.AuthorId = authorId;
+                book.GenreId = genreId;
+                book.PublishedYear = publishedYear;
+                _unitOfWork.Books.Update(book);
+                _unitOfWork.Commit();
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            var book = _unitOfWork.Books.GetById(id);
+            if (book != null)
+            {
+                _unitOfWork.Books.Delete(book);
+                _unitOfWork.Commit();
+            }
+        }
+
+        public IEnumerable<Book> GetBooksByGenre(int genreId) =>
+            _unitOfWork.Books.GetAll().Where(b => b.GenreId == genreId);
+
+        public IEnumerable<Book> GetBooksByAuthor(int authorId) =>
+            _unitOfWork.Books.GetAll().Where(b => b.AuthorId == authorId);
+    }
+}
