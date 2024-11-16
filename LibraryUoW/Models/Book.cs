d1c40f62fe54bbae127fca78868269c51765e1f2
Анна <vindanka@gmail.com>
2024-11-16 22:27:32 +0200

    Create models and connect to DB

---
+++ LibraryUoW/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryUoW.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public int PublishedYear { get; set; }
+    }
+}
---
+++ LibraryUoW/Models/BooksContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryUoW.Models
+{
+    public class BooksContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Users\\user\\Documents\\LibraryUoW.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasKey(b => b.Id);
+
+            modelBuilder.Entity<Author>()
+                .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Genre>()
+                .HasKey(g => g.Id);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Genre)
+                .WithMany(g => g.Books)
+                .HasForeignKey(b => b.GenreId);
+        }
+    }
+}
